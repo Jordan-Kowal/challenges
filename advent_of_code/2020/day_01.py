7c8aba37fe5dd680185f2f50f46c01f2c331f2ba
Jordan Kowal <kowaljordan@gmail.com>
2020-12-02 19:53:49 +0100

    [FEAT] AOC2020: Day 2 challenge

--- advent_of_code/2020/day_01.py
+++ advent_of_code/2020/day_01.py
@@ -1,6 +1,5 @@
 """Day 1 challenge"""
 
-
 # Built-in
 from itertools import combinations
 
@@ -11,6 +10,14 @@ from _shared import read_input
 # > Main
 # --------------------------------------------------------------------------------
 values = [int(v) for v in read_input("day_01.txt")]
+
+# First problem
+for a, b in combinations(values, 2):
+    if (a + b) == 2020:
+        print(a * b)
+        break
+
+# Second problem
 for a, b, c in combinations(values, 3):
     if (a + b + c) == 2020:
         print(a * b * c)
---
+++ advent_of_code/2020/day_02.py
@@ -0,0 +1,43 @@
+"""Day 1 challenge"""
+
+# Built-in
+import re
+
+# Personal
+from _shared import read_input
+
+# --------------------------------------------------------------------------------
+# > Main
+# --------------------------------------------------------------------------------
+REGEX = r"^(\d+)-(\d+) ([a-z]): (.+)$"
+
+
+def parse_line(input_line):
+    """
+    Parses the input line to extract its information
+    :param str input_line: Line from the input file
+    :return: The first number, second number, enforced letter, and the password
+    :rtype: (int, int, str, str)
+    """
+    match = re.match(REGEX, input_line)
+    return (
+        int(match.group(1)),
+        int(match.group(2)),
+        match.group(3),
+        match.group(4),
+    )
+
+
+first_total, second_total = 0, 0
+for line in read_input("day_02.txt"):
+    min_, max_, letter, text = parse_line(line)
+    # First problem
+    letter_count = text.count(letter)
+    if min_ <= letter_count <= max_:
+        first_total += 1
+    # Second problem
+    first = text[min_ - 1]
+    second = text[max_ - 1]
+    if (first == letter or second == letter) and first != second:
+        second_total += 1
+print(first_total, second_total)
