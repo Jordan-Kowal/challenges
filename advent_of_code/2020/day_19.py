d748948b6cf879900e8003fc19962d5f9147b627
Jordan Kowal <kowaljordan@gmail.com>
2020-12-19 20:41:18 +0100

    [FEAT] AOC2020: Added storage to day19 regexes

--- advent_of_code/2020/day_19.py
+++ advent_of_code/2020/day_19.py
@@ -11,22 +11,32 @@ from _shared import read_input
 # --------------------------------------------------------------------------------
 # > Helpers
 # --------------------------------------------------------------------------------
-RECURSION_LEVEL = 5
+RECURSION_LEVEL = 6
 
 
 def compute_rule_regex(rule_id, problem):
     """
     Recursively build regexes based on the given rules
+    Fills the REGEXES dict to avoid computing the same rule several times
     :param str rule_id: ID of the rule we want
     :param int problem: The AOC problem we are doing (1 or 2)
     :return:
     :rtype: str
     """
+    # Escape case
+    rule = RULES[rule_id]
+    if rule in "ab":
+        return rule
+    # Already done
+    if rule_id in REGEXES:
+        return REGEXES[rule_id]
     # Special handling of problem 2
     if problem == 2:
         # Repeat rule 42 1 or more times
         if rule_id == "8":
-            return compute_rule_regex("42", 2) + "+"
+            result = compute_rule_regex("42", 2) + "+"
+            REGEXES[rule_id] = result
+            return result
         # Can have several 42s followed by the same amount of 31s
         if rule_id == "11":
             r1 = compute_rule_regex("42", problem)
@@ -35,23 +45,22 @@ def compute_rule_regex(rule_id, problem):
                 "{" + str(i) + "}" for i in range(1, RECURSION_LEVEL + 1)
             ]  # DO NOT START AT 0 JFC
             combinaisons = [rf"{r1}{q}{r2}{q}" for q in quantities]
-            return rf"({'|'.join(combinaisons)})"
-    # Escape case
-    rule = RULES[rule_id]
-    if rule in "ab":
-        return rule
+            result = rf"({'|'.join(combinaisons)})"
+            REGEXES[rule_id] = result
+            return result
     # Recursively build the sub-rules
-    else:
-        groups = rule.split("|")
-        result = []
-        # Merge using AND
-        for group in groups:
-            ids = group.strip().split(" ")
-            sub_regexes = [compute_rule_regex(id_, problem) for id_ in ids]
-            merged_regex = "".join(sub_regexes)
-            result.append(merged_regex)
-        # Merge using OR
-        return rf"({'|'.join(result)})"
+    groups = rule.split("|")
+    result = []
+    # Merge using AND
+    for group in groups:
+        ids = group.strip().split(" ")
+        sub_regexes = [compute_rule_regex(id_, problem) for id_ in ids]
+        merged_regex = "".join(sub_regexes)
+        result.append(merged_regex)
+    # Merge using OR
+    result = rf"({'|'.join(result)})"
+    REGEXES[rule_id] = result
+    return result
 
 
 def split_inputs(content):
@@ -83,6 +92,7 @@ raw_rules, texts = split_inputs(content)
 
 # Builds rules dict
 RULES = {}
+REGEXES = {}
 for input in raw_rules:
     rule_id, rule_text = input.split(": ")
     rule_text = re.sub('"', "", rule_text)
