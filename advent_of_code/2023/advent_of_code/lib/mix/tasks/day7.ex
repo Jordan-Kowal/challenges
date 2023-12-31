c5737cdaab48bd2070a243d414e4ebe8617145c4
Jordan Kowal <kowaljordan@gmail.com>
2023-12-31 15:44:29 +0100

    feat: aoc day7 p1

---
+++ advent_of_code/2023/advent_of_code/lib/mix/tasks/day7.ex
@@ -0,0 +1,100 @@
+defmodule Mix.Tasks.Day7 do
+  @moduledoc "Day 7"
+
+  use AdventOfCode.DayTask
+
+  @type hand :: [String.t()]
+  @type hand_type ::
+          :five_of_a_kind
+          | :four_of_a_kind
+          | :full_house
+          | :three_of_a_kind
+          | :two_pairs
+          | :one_pair
+          | :high_card
+  @type bid :: integer()
+  @type hand_data :: {hand(), bid(), hand_type()}
+
+  @types %{
+    five_of_a_kind: 6,
+    four_of_a_kind: 5,
+    full_house: 4,
+    three_of_a_kind: 3,
+    two_pairs: 2,
+    one_pair: 1,
+    high_card: 0
+  }
+
+  @impl AdventOfCode.DayTask
+  def solve_p1(lines), do: solve(lines, false)
+
+  @impl AdventOfCode.DayTask
+  def solve_p2(lines), do: solve(lines, true)
+
+  @spec solve([String.t()], boolean()) :: integer()
+  defp solve(lines, jack_as_joker) do
+    lines
+    |> Enum.map(fn line -> parse_line(line, jack_as_joker) end)
+    |> Enum.sort(fn cards1, cards2 -> compare_hands(cards1, cards2, jack_as_joker) end)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.map(fn {{_, bid, _}, index} -> (index + 1) * bid end)
+    |> Enum.sum()
+  end
+
+  @spec parse_line(String.t(), boolean()) :: hand_data()
+  defp parse_line(line, jack_as_joker) do
+    [value, string_bid] = String.split(line, " ")
+    hand = String.graphemes(value)
+    {hand, String.to_integer(string_bid), compute_hand_type(hand, jack_as_joker)}
+  end
+
+  @spec compute_hand_type(hand(), boolean()) :: hand_type()
+  defp compute_hand_type(hand, _) do
+    hand
+    |> Enum.frequencies()
+    |> Map.values()
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> case do
+      [5] -> :five_of_a_kind
+      [4, 1] -> :four_of_a_kind
+      [3, 2] -> :full_house
+      [3 | _] -> :three_of_a_kind
+      [2, 2 | _] -> :two_pairs
+      [2 | _] -> :one_pair
+      _ -> :high_card
+    end
+  end
+
+  @spec compare_hands(hand_data(), hand_data(), boolean()) :: boolean()
+  defp compare_hands({hand1, _, type1}, {hand2, _, type1}, jack_as_joker) do
+    hand1
+    |> Enum.zip(hand2)
+    |> Enum.reduce_while(false, fn {card1, card2}, acc ->
+      card1_value = card_to_value(card1, jack_as_joker)
+      card2_value = card_to_value(card2, jack_as_joker)
+
+      cond do
+        card1_value > card2_value -> {:halt, true}
+        card1_value < card2_value -> {:halt, false}
+        true -> {:cont, acc}
+      end
+    end)
+  end
+
+  defp compare_hands({_, _, type1}, {_, _, type2}, _) do
+    @types[type1] > @types[type2]
+  end
+
+  defp card_to_value(card, jack_as_joker) do
+    case card do
+      "A" -> 14
+      "K" -> 13
+      "Q" -> 12
+      "J" -> if jack_as_joker, do: 11, else: 1
+      "T" -> 10
+      _ -> String.to_integer(card)
+    end
+  end
+end
